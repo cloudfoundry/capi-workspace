84936c804d7fe755a6426a77e4d3c048b55d6a76
Raina Masand <rmasand@pivotal.io>
2018-02-12 10:32:46 -0800

    Add Brewfile and script to install
    Just ported over the list from sprout-capi

---
+++ brew_bundle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+capi_brewfile="${PWD}/Brewfile"
+brewfile_link="${HOME}/.Brewfile"
+if [ ! -L "${brewfile_link}" ]; then
+  echo "Creating Brewfile symlink"
+  ln -s "${capi_brewfile}" "${brewfile_link}"
+fi
+
+echo "Installing from the Brewfile..."
+brew update
+brew tap Homebrew/bundle
+brew bundle --global
+brew cleanup
