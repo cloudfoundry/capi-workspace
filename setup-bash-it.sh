45d925be86a01fd1febcbc84148730f6048afac6
Greg Cobb <gcobb@pivotal.io>
2018-02-21 10:50:10 -0800

    Add script for installing bash_it
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

---
+++ setup-bash-it.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+function install_bash_it {
+	if ! type bash_it > /dev/null 2>&1 ; then
+		echo "Installing bash_it"
+		if [ ! -e ~/.bash_it ] ; then
+			git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+		fi
+		~/.bash_it/install.sh --silent
+		source ~/.bash_profile
+	else
+		echo "bash_it already installed"
+	fi
+}
+
+install_bash_it
