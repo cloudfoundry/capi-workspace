5f98d4d1a2729e03a8f38cd434abe36f22fa926d
Greg Cobb <gcobb@pivotal.io>
2018-02-23 11:00:53 -0800

    Add more repos to git clone
    [#155295224]

--- clone-repos.sh
+++ clone-repos.sh
@@ -19,5 +19,8 @@ pushd ~/workspace > /dev/null
 
 	clone git@github.com:pivotal/pivotal_ide_prefs.git ~/workspace/pivotal_ide_prefs
 	clone git@github.com:cloudfoundry/capi-release.git ~/workspace/capi-release develop
+	clone git@github.com:cloudfoundry/capi-env-pool.git ~/workspace/capi-env-pool
+	clone git@github.com:cloudfoundry/capi-ci.git ~/workspace/capi-ci
+	clone git@github.com:cloudfoundry/capi-ci-private.git ~/workspace/capi-ci-private
 
 popd > /dev/null
