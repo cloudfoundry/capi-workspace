a9b656f437fc197a371e6094e3f2d59bbff19469
Eric Promislow <eric.promislow@suse.com>
2018-02-27 17:08:25 -0800

    Be more cautious when configuring git
    * Copy the exiting .gitconfig to .gitconfig.bak
    
    * Only copy over the sections in the supplied config, so things
      like github token info, settings, names survive.

---
+++ helpers/transfer-git-aliases.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# Transfer only sections from the capi-workspace-managed gitconfig
+# Leave the other parts in place for OSS contributors.
+
+require 'inifile'
+
+source_path = ARGV[0]
+target_path = ARGV[1]
+
+source = IniFile.load(source_path)
+target = IniFile.load(target_path)
+
+source.sections.each do |section_name|
+  target[section_name] = source[section_name]
+end
+target.write
+  
--- setup-git-config.sh
+++ setup-git-config.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-ln -fs "${PWD}/assets/gitconfig" "${HOME}/.gitconfig"
+SOURCE="${PWD}/assets/gitconfig"
+TARGET="${HOME}/.gitconfig"
+
+if [ ! -e "${TARGET}" ] ; then
+    ln -s "${SOURCE}" "${TARGET}"
+elif diff --brief "${SOURCE}" "${TARGET}" ; then
+    echo "no change needed for \"${TARGET}\""
+else
+    echo Copying \"${TARGET}\" to \"${TARGET}.bak\"
+    cp "${TARGET}" "${TARGET}.bak"
+    ruby -rinifile -e 1 2>/dev/null || gem install inifile
+    ruby -rinifile "${PWD}/helpers/transfer-git-aliases.rb" "${SOURCE}" "${TARGET}"
+fi
