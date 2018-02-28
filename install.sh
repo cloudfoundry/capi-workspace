3b4d4072ad3955a1b36ceef092714843204b5fef
Raina Masand <rmasand@pivotal.io>
2018-02-27 18:25:55 -0800

    Reorganize and label the install script
    In case things are in order for a reason, we can mention it

--- install.sh
+++ install.sh
@@ -8,19 +8,38 @@ source ./install-brew.sh
 source ./install-xcode.sh
 source ./brew-bundle.sh
 
+# install specific version of consul that's compatible with cc bridge
+# there's a tech forum item to discuss whether cc bridge tests should still rely on consul
+source ./install-consul.sh
+
+# bash-it / terminal
 source ./setup-bash-it.sh
+source ./setup-custom-bash-it-plugins.sh
+source ./setup-iterm2.sh
+source ./setup-vim.sh
+
+# git setup
 source ./setup-git-config.sh
 source ./setup-git-duet.sh
-source ./clone-repos.sh
+
+# ruby setup
 source ./setup-ruby.sh
 source ./setup-rubymine.sh
+
 source ./setup-keyboard.sh
-source ./setup-custom-bash-it-plugins.sh
+source ./setup-spectacle.sh
+
+# daemons to launch databases at startup
 source ./setup-mysql.sh
 source ./setup-postgres.sh
-source ./setup-iterm2.sh
+
+
+# Golang setup
 source ./setup-go.sh
-source ./setup-vim.sh
-source ./setup-spectacle.sh
+
+# Depends on existence of GOPATH, created earlier on
+source ./clone-repos.sh
+
+source ./setup-cats.sh
 
 echo "Success!!"
