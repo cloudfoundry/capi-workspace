57d3fa40c425fca5c8507735edbb1f9e5826475f
Charles Hansen <chansen@pivotal.io>
2018-03-26 15:08:37 -0700

    Clone pivotal_ide_prefs if they do not already exist
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- setup-rubymine.sh
+++ setup-rubymine.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ ! -d ~/workspace/pivotal_ide_prefs ]; then
+	git clone https://github.com/pivotal/pivotal_ide_prefs.git ~/workspace/pivotal_ide_prefs
+fi
+
 pushd ~/workspace/pivotal_ide_prefs > /dev/null
 	./cli/bin/ide_prefs install --ide=rubymine
 popd > /dev/null
