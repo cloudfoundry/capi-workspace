2e8d64adbc53e2fc60c3f40129bd362417f6714e
Greg Cobb <gcobb@pivotal.io>
2018-04-03 17:18:12 -0700

    Use a smaller dock
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

--- setup-dock.sh
+++ setup-dock.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 set +e
-defaults delete com.apple.dock persistent-apps && killall Dock
+defaults delete com.apple.dock persistent-apps >/dev/null 2>&1 || true
+# Default height probably floor(160 / 2 - 11) = 69
+defaults write com.apple.dock tilesize -int 29
+killall Dock
 set -e
