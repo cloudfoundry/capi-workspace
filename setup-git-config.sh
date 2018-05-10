5942547a707e13c52e96b697f948c8acc707ad9e
Chris Selzo <cselzo@pivotal.io>
2018-05-10 09:20:00 -0700

    No longer symlink gitconfig
    - If there is a symlink, remove it
    - Copy the file from capi-workspace if it doesn't exist
    - If it does exist, back it up and then just copy over the sections in
    the config template

--- setup-git-config.sh
+++ setup-git-config.sh
@@ -5,13 +5,20 @@ set -e
 SOURCE="${PWD}/assets/gitconfig"
 TARGET="${HOME}/.gitconfig"
 
-if [ ! -e "${TARGET}" ] ; then
-    ln -s "${SOURCE}" "${TARGET}"
-elif diff --brief "${SOURCE}" "${TARGET}" ; then
-    echo "no change needed for \"${TARGET}\""
-else
+if [ -L "${TARGET}" ]; then
+		# If the global git config is symlinked, remove the symlink
+    unlink "${TARGET}"
+fi
+
+if [ -e "${TARGET}" ]; then
+		# If the global git config exists, back it up before modifying
     echo Copying \"${TARGET}\" to \"${TARGET}.bak\"
     cp "${TARGET}" "${TARGET}.bak"
-    ruby -rinifile -e 1 2>/dev/null || gem install inifile
-    ruby -rinifile "${PWD}/helpers/transfer-git-aliases.rb" "${SOURCE}" "${TARGET}"
+else
+		# If the global git config doesn't exist, copy it from capi-workspace
+    cp "${SOURCE}" "${TARGET}"
 fi
+
+# Copy the aliases from the git config stored in capi-workspace to the global git config
+ruby -rinifile -e 1 2>/dev/null || gem install inifile
+ruby -rinifile "${PWD}/helpers/transfer-git-aliases.rb" "${SOURCE}" "${TARGET}"
