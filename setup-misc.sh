0765a64dcc72fa8925f1fc93ce481661adc2da6c
Tom Viehman <tviehman@pivotal.io>
2018-05-14 10:31:54 -0700

    Fixing fixcomitter; remember Tom.
    Signed-off-by: Davis W. Frank <dwfrank@pivotal.io>

--- setup-misc.sh
+++ setup-misc.sh
@@ -6,6 +6,6 @@ set -e
 TARGET=/usr/local/bin
 SOURCE=$HOME/workspace/capi-release/scripts # notice this NOT capi-workspace, if you are adding content to this file (REFACTOR!)
 
-if [ ! -f $SOURCE/fixcommitter ] ; then
+if [ ! -f $TARGET/fixcommitter ] ; then
 	ln -s $SOURCE/fixcommitter $TARGET/fixcommitter
 fi
