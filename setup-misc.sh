2d372369366051c0fe30e0d582d6efd558459e87
Chris Selzo <cselzo@pivotal.io>
2018-05-15 09:48:36 -0700

    Only link fixcommitter if it doesn't exist

--- setup-misc.sh
+++ setup-misc.sh
@@ -6,6 +6,6 @@ set -e
 TARGET=/usr/local/bin
 SOURCE=$HOME/workspace/capi-release/scripts # notice this NOT capi-workspace, if you are adding content to this file (REFACTOR!)
 
-if [ ! -f $TARGET/fixcommitter ] ; then
+if [ ! -h $TARGET/fixcommitter ] && [ ! -f $TARGET/fixcommitter ]; then
 	ln -s $SOURCE/fixcommitter $TARGET/fixcommitter
 fi
