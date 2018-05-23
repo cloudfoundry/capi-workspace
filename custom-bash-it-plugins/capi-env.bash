45fb3d5ae77722bf34b5d3dd5768ce6945c29863
Lisa Cho <lcho@pivotal.io>
2018-05-23 15:07:58 -0700

    Fix python installation
    Now that python3 is homebrew's default, this had to be reconfigured.
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- custom-bash-it-plugins/capi-env.bash
+++ custom-bash-it-plugins/capi-env.bash
@@ -9,3 +9,7 @@ export EDITOR=nvim
 # Setup GOPATH
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
+
+# Add brew-installed python to the path.
+# Must go at the beginning so that it's prioritized over the system installation.
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
