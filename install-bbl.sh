78a759c0f0aad50164644d113664f76ac800c772
Tim Downey <tdowney@pivotal.io>
2018-05-24 10:45:19 -0700

    Update install-bbl.sh script to install bbl 4 and 6
    - the binary previously known as bbl is now installed as bbl4
    - the bbl binary is now bbl version 6
    
    [#155957637]

--- install-bbl.sh
+++ install-bbl.sh
@@ -5,12 +5,19 @@ set -e
 # This will not be needed once we catch up with the bbl version on brew
 # Then we can install bbl using the Brewfile
 function install_bbl {
-	if ! which bbl > /dev/null ; then
-                echo "Installing bbl"
-		local destination=/usr/local/bin/bbl
-		wget "https://github.com/cloudfoundry/bosh-bootloader/releases/download/v4.10.4/bbl-v4.10.4_osx" -O $destination
+	if ! which bbl4 > /dev/null ; then
+                echo "Installing bbl 4"
+		local destination=/usr/local/bin/bbl4
+		wget "https://github.com/cloudfoundry/bosh-bootloader/releases/download/v4.10.5/bbl-v4.10.5_osx" -O $destination
 		chmod +x $destination
 	fi
+
+	if ! which bbl > /dev/null ; then
+                  echo "Installing bbl"
+    local destination=/usr/local/bin/bbl
+    wget "https://github.com/cloudfoundry/bosh-bootloader/releases/download/v6.6.11/bbl-v6.6.11_osx" -O $destination
+    chmod +x $destination
+  fi
 }
 
 install_bbl
