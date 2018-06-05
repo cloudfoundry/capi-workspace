973f8381b47973e497d9af4e61c394de19dbe58b
Jeff Pak <jeffrey.pak@dell.com>
2018-06-05 10:20:53 -0700

    update gem to 2.6.14 if necessary
    [#158015952]
    
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

--- setup-ruby.sh
+++ setup-ruby.sh
@@ -8,6 +8,11 @@ if [ ! -e ~/.rubies/ruby-2.4.2 ]; then
 	ruby-install ruby 2.4.2
 fi
 echo "ruby-2.4.2" > ~/.ruby-version
+ruby -e 'if (`gem --version`.chomp.split(".").map(&:to_i) <=> [2, 6, 14]) == -1 ; \
+	system("gem install rubygems-update") ; \
+	system("update_rubygems"); \
+	system("gem update --system 2.6.14") \
+end'
 enable_bash_it_plugin chruby-auto
 set +e
 source ~/.bash_profile
