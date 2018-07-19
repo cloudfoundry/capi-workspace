f5a7485658ee4f4487a25b7e32de1cd635906789
Davis W. Frank <dwfrank@pivotal.io>
2018-07-19 10:09:16 -0700

    Restore create_and_* aliases, fix capidoc.yml
    Signed-off-by: Tim Downey <tdowney@pivotal.io>

--- custom-bash-it-plugins/capi-aliases.bash
+++ custom-bash-it-plugins/capi-aliases.bash
@@ -27,3 +27,6 @@ alias relite='(target_cf && cf rp retest)'
 
 # ci
 alias watch_latest_cc_units='fly -t capi watch -j capi/cc-unit-tests'
+
+alias create_and_deploy='create_and_upload && ~/workspace/capi-release/scripts/deploy'
+alias create_and_force_deploy='create_and_upload && ~/workspace/capi-release/scripts/deploy -n'
