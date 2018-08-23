d26bd597c277afd9cea04527052818900893836f
Eric Promislow <eric.promislow@suse.com>
2018-08-23 13:51:52 -0700

    Make `g` a pseudo-alias for both `git status` and `git`
    Really, best to just type 'git'

--- custom-bash-it-plugins/capi-aliases.bash
+++ custom-bash-it-plugins/capi-aliases.bash
@@ -7,7 +7,16 @@ alias bake='bundle exec rake'
 # Git aliases
 alias gd='git diff'
 alias gdc='git diff --cached'
-alias g='git status'
+
+# capi likes `alias g="git status"`, other teams like `alias g=git`, so do both:
+alias g 2>/dev/null && unalias g
+
+function g() {
+    case $# in
+	0) git status ;;
+	*) git "$@" ;;
+    esac
+}
 
 # Misc aliases
 alias gi='gem install'
