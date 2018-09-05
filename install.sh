a2491dd2e59fe2f18916e51a57573ac5aab15215
Charles Hansen <chansen@pivotal.io>
2018-09-05 10:25:01 -0700

    Move our install scripts to enable tab completion
    Now you can just type ./in<TAB> and be happy!
    
    Signed-off-by: Mike Kenyon <mkenyon@pivotal.io>

--- install.sh
+++ install.sh
@@ -3,64 +3,65 @@
 set -e
 
 function install {
+
 # install brew and its packages
-source ./install-brew.sh
-source ./install-xcode.sh
+source ./setup/brew.sh
+source ./setup/xcode.sh
 source ./brew-bundle.sh
 
 # install specific version of consul that's compatible with cc bridge
-source ./install-consul.sh
+source ./setup/consul.sh
 
 # bash-it / terminal
-source ./setup-bash-it.sh
-source ./setup-custom-bash-it-plugins.sh
-source ./setup-iterm2.sh
-source ./setup-vim.sh
-source ./setup-jarg.sh
+source ./setup/bash-it.sh
+source ./setup/custom-bash-it-plugins.sh
+source ./setup/iterm2.sh
+source ./setup/vim.sh
+source ./setup/jarg.sh
 
 # ruby setup
-source ./setup-ruby.sh
-source ./install-bundler.sh
-source ./install-uaac.sh
+source ./setup/ruby.sh
+source ./setup/bundler.sh
+source ./setup/uaac.sh
 
 # git setup
-source ./setup-git-config.sh
-source ./setup-git-duet.sh
-source ./setup-git-hooks.sh
+source ./setup/git-config.sh
+source ./setup/git-duet.sh
+source ./setup/git-hooks.sh
 
 # ide prefs
-source ./setup-ide-prefs.sh
+source ./setup/ide-prefs.sh
 
-source ./setup-keyboard.sh
-source ./setup-dock.sh
-source ./setup-spectacle.sh
+source ./setup/keyboard.sh
+source ./setup/dock.sh
+source ./setup/spectacle.sh
 
 # daemons to launch databases at startup
-source ./setup-mysql.sh
-source ./setup-postgres.sh
+source ./setup/mysql.sh
+source ./setup/postgres.sh
 
 # Golang setup
-source ./setup-go.sh
+source ./setup/go.sh
 
 # Depends on existence of GOPATH, created earlier on
 source ./clone-repos.sh
 
-source ./setup-cats.sh
+source ./setup/cats.sh
 
-source ./install-fly.sh
+source ./setup/fly.sh
 
-source ./setup-cron.sh
+source ./setup/cron.sh
 
-source ./setup-misc.sh
+source ./setup/misc.sh
 
 # Instal CLI cf-httpie plugin
-source ./setup-httpie.sh
+source ./setup/httpie.sh
 
 # Add gem dependencies for CAPI-Workspace
 bundle
 
 # Index capi.land links in Spotlight
-./setup-capi-land-links
+./setup/capi-land-links.rb
 }
 
 
