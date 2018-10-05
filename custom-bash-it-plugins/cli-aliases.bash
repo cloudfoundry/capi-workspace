3b69faf9765e22eb79a8f8dde4c9baaeca664061
Mike Kenyon <mkenyon@pivotal.io>
2018-10-05 10:41:07 -0700

    Add alias `int` to quickly set CLI integration test target
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

---
+++ custom-bash-it-plugins/cli-aliases.bash
@@ -0,0 +1,1 @@
+ alias int="export CF_INT_API=https://api.$BOSH_LITE_DOMAIN; credhub login --skip-tls-validation; CF_INT_PASSWORD=$(credhub get --name '/bosh-lite/cf/cf_admin_password' --output-json | jq -r '.value')"
