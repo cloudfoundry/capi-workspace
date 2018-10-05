14381c1d95dafc0efc00f71e2ab9e14a7ddc8e0e
Eli Wrenn <ewrenn@pivotal.io>
2018-10-05 11:17:29 -0700

    Use function instead of alias
    The BOSH_LITE_DOMAIN was being evaluated when opening a terminal, as in,
    way too early.
    
    Signed-off-by: Mike Kenyon <mkenyon@pivotal.io>

--- custom-bash-it-plugins/cli-aliases.bash
+++ custom-bash-it-plugins/cli-aliases.bash
@@ -1,1 +1,6 @@
- alias int="export CF_INT_API=https://api.$BOSH_LITE_DOMAIN; credhub login --skip-tls-validation; CF_INT_PASSWORD=$(credhub get --name '/bosh-lite/cf/cf_admin_password' --output-json | jq -r '.value')"
+function int() {
+	export CF_INT_API=https://api.$BOSH_LITE_DOMAIN
+
+	credhub login --skip-tls-validation
+	export CF_INT_PASSWORD=$(credhub get --name '/bosh-lite/cf/cf_admin_password' --output-json | jq -r '.value')
+}
