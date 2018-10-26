2a127b155b724d5dddfcf28028431e80dd5c1858
Anand Gaitonde <agaitonde@pivotal.io>
2018-10-26 10:27:12 -0700

    handle duplicate files when copying iterm2 settings

--- setup/iterm2.sh
+++ setup/iterm2.sh
@@ -4,4 +4,6 @@ set -e
 current_directory=$PWD
 preferences_path="/Users/$(whoami)/Library/Preferences/com.googlecode.iterm2.plist"
 
-cp -f "${current_directory}/assets/com.googlecode.iterm2.plist" "${preferences_path}"
+if ! diff "${current_directory}/assets/com.googlecode.iterm2.plist" "${preferences_path}"; then
+	cp -f "${current_directory}/assets/com.googlecode.iterm2.plist" "${preferences_path}"
+fi
