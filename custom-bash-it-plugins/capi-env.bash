97193f9249fb580565b74a410d60f767f2f31b83
Spencer Hawley <shawley@pivotal.io>
2018-12-17 15:33:20 -0800

    don't manually add python3 to import path, bash-it does this
    - this additionally messes with neovim when it looks up python3
    
    Signed-off-by: Anand Gaitonde <agaitonde@pivotal.io>

--- custom-bash-it-plugins/capi-env.bash
+++ custom-bash-it-plugins/capi-env.bash
@@ -18,4 +18,4 @@ export PATH="$PATH:$HOME/workspace/capi-workspace/scripts"
 
 # Add brew-installed python to the path.
 # Must go at the beginning so that it's prioritized over the system installation.
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+# export PATH="/usr/local/opt/python/libexec/bin:$PATH"
