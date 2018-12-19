266a54c9ad37232c5df7a007d213e170a5593e3e
Anand Gaitonde <agaitonde@pivotal.io>
2018-12-19 12:07:36 -0800

    always use postgres, cuz it's just better, you know it
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

--- custom-bash-it-plugins/capi-env.bash
+++ custom-bash-it-plugins/capi-env.bash
@@ -19,3 +19,6 @@ export PATH="$PATH:$HOME/workspace/capi-workspace/scripts"
 # Add brew-installed python to the path.
 # Must go at the beginning so that it's prioritized over the system installation.
 # export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+# always use postgres for tests
+export DB=postgres
