63f4b8f069f9365977014c01f7c9a45e78c81167
Tim Downey <tdowney@pivotal.io>
2019-01-08 12:11:09 -0800

    Bump ruby version to 2.4.5
    Signed-off-by: Chris Selzo <cselzo@pivotal.io>

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -4,10 +4,12 @@ set -e
 
 source ./helpers/bash-it-helpers.sh
 
-if [ ! -e ~/.rubies/ruby-2.4.2 ]; then
-	ruby-install ruby 2.4.2
+RUBY_VERSION="2.4.5"
+
+if [ ! -e ~/.rubies/ruby-${RUBY_VERSION} ]; then
+	ruby-install ruby ${RUBY_VERSION}
 fi
-echo "ruby-2.4.2" > ~/.ruby-version
+echo "ruby-${RUBY_VERSION}" > ~/.ruby-version
 ruby -e 'if (`gem --version`.chomp.split(".").map(&:to_i) <=> [2, 6, 14]) == -1 ; \
 	system("gem install rubygems-update") ; \
 	system("update_rubygems"); \
@@ -17,4 +19,4 @@ enable_bash_it_plugin chruby-auto
 set +e
 source ~/.bash_profile
 set -e
-chruby 2.4.2
+chruby ${RUBY_VERSION}
