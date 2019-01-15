73c8eb1ef3ba60e5a49dbcd38bd3e53b3f0ff4a3
Eric Promislow <eric.promislow@suse.com>
2019-01-15 14:10:33 -0800

    fixup, remove not-strictly necessary fix for hey
    Signed-off-by: Connor Braa <cbraa@pivotal.io>

--- setup/go.sh
+++ setup/go.sh
@@ -12,26 +12,16 @@ if [ -L ${go_src} ]; then
 fi
 
 GO_UTILS=(
-	code.cloudfoundry.org/perm/cmd/perm
-  github.com/alecthomas/gometalinter
-  github.com/jteeuwen/go-bindata/...
+  github.com/rakyll/hey
+  code.cloudfoundry.org/perm/cmd/perm
   github.com/maxbrunsfeld/counterfeiter
   github.com/onsi/ginkgo/ginkgo
   github.com/onsi/gomega
   github.com/XenoPhex/i18n4go/i18n4go
 )
 
-GO_UTILS_USING_MODULES=(
-  github.com/rakyll/hey
-)
-
 echo "Running $(go version)"
 for gopkg in "${GO_UTILS[@]}"; do
   echo "Getting/Updating $gopkg"
   GOPATH=$HOME/go go get -u $gopkg
-done
-
-for gopkg in "${GO_UTILS_USING_MODULES[@]}"; do
-  echo "Getting/Updating $gopkg"
-  GOPATH=$HOME/go GO111MODULE=on go get -u $gopkg
 done
