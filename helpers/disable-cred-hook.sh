77e834fa94f0c37a69bfdea23f1ea46a1d46bbcf
Connor Braa <cbraa@pivotal.io>
2019-01-22 12:12:45 -0800

    disable cred checking hooks for private repos
    Signed-off-by: Eric Promislow <eric.promislow@suse.com>

---
+++ helpers/disable-cred-hook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+function disable_cred_hook {
+  mkdir -p "${1}/.git/hooks"
+  cat > "${1}/.git/hooks/post-merge" << EOT
+#!/usr/bin/env/bash
+
+# Override the default hook that complains about credentials
+EOT
+  chmod +x "${1}/.git/hooks/post-merge"
+}
