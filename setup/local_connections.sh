f6206cd03c54fa3548ea138a4e6b55d557bd6017
Eric Promislow <eric.promislow@suse.com>
2019-03-25 10:25:27 -0700

    Don't fail on failing grep

--- setup/local_connections.sh
+++ setup/local_connections.sh
@@ -1,4 +1,4 @@
-x=$(grep --no-messages --no-filename CONNECTION_PREFIX ~/.profile ~/.bash_profile ~/.bashrc)
+x=$(grep --no-messages --no-filename CONNECTION_PREFIX ~/.profile ~/.bash_profile ~/.bashrc || true)
 if [ -n "$x" ] ; then
   eval "$x"
 fi
