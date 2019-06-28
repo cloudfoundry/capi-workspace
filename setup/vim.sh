0107121644f5746582c87c3bb2dfb0a7460b6388
Connor Braa <cbraa@pivotal.io>
2019-06-28 11:52:05 -0700

    switch to shiny new luan nvim
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- setup/vim.sh
+++ setup/vim.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 set -e
 
-# this is where new luan nvim installs to, so delete it to revert
-rm -rf ~/.config/nvim/
-
-if [[ ! -d ~/.vim ]]; then
-	curl vimfiles.luan.sh/install | bash
-fi
-
-pushd ~/.vim
-if git remote -v | grep -q luan &> /dev/null; then
-	echo $PWD
-	./install
-fi
-popd
+grep -q "github.com/luan/nvim" ~/.config/nvim/.git/config ||
+	(rm -rf ${HOME}/.config/nvim ${HOME}/.vim* ${HOME}/.local/share/nvim &&
+	git clone https://github.com/luan/nvim ~/.config/nvim)
 
 echo "Update pip..."
 pip3 install --upgrade pip
 
 echo "Install python-client for neovim..."
-pip3 install neovim
+pip3 install --upgrade neovim
 
 echo "Add yamllint for neomake..."
 pip3 install -q yamllint
+
+mkdir -p ~/.config/nvim/user
+pushd ~/.config/nvim/user
+	ln -sf ~/workspace/capi-workspace/assets/*.vim .
+popd
