40d951aba4a8fa1c9bc239167427ab9cd1c95239
Reid Mitchell <rmitchell@pivotal.io>
2019-09-17 16:51:33 -0700

    Let people have the dock size they want. Add system preferences to dock.

--- setup/dock.sh
+++ setup/dock.sh
@@ -2,7 +2,6 @@
 
 set +e
 defaults delete com.apple.dock persistent-apps >/dev/null 2>&1 || true
-# Default height probably floor(160 / 2 - 11) = 69
-defaults write com.apple.dock tilesize -int 29
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/System Preferences.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 killall Dock
 set -e
