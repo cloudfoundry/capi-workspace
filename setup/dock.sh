6204ddd82ea08556a759147920dbd6d01e861ee5
Belinda Liu <bliu@pivotal.io>
2019-10-21 15:57:36 -0700

    Remove dock configuration script
    give the people the dock size they want.
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>
    Co-authored-by: Teal Stannard <tstannard@pivotal.io>

--- setup/dock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set +e
-defaults delete com.apple.dock persistent-apps >/dev/null 2>&1 || true
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/System Preferences.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-killall Dock
-set -e
