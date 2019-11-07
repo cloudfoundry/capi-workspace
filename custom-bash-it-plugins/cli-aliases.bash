bad33ab6ed46b8b248363cc65a8ab809f1cf7de7
Reid Mitchell <rmitchell@pivotal.io>
2019-11-07 10:52:06 -0800

    remove exit 1 from int script
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>
    Co-authored-by: Merric de Launey <mdelauney@pivotal.io>

--- custom-bash-it-plugins/cli-aliases.bash
+++ custom-bash-it-plugins/cli-aliases.bash
@@ -1,11 +1,10 @@
 function int() {
   if [ -z "$BOSH_ENVIRONMENT" ]; then
     echo "No bosh targeted. Use \"target_bosh\" before trying again"
-    exit 1
+  else
+    TARGETED_BOSH_LITE_NAME=$(echo $BOSH_LITE_DOMAIN | grep lite.cli.fun | cut -d. -f1)
+    echo "Loading integration environment variables for $TARGETED_BOSH_LITE_NAME..."
+
+    source ~/workspace/cli-private/set_int_test_lite.sh $TARGETED_BOSH_LITE_NAME
   fi
-
-	TARGETED_BOSH_LITE_NAME=$(echo $BOSH_LITE_DOMAIN | grep lite.cli.fun | cut -d. -f1)
-  echo "Loading integration environment variables for $TARGETED_BOSH_LITE_NAME..."
-
-  source ~/workspace/cli-private/set_int_test_lite.sh $TARGETED_BOSH_LITE_NAME
 }
