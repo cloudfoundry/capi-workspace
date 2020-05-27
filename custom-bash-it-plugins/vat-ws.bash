85c3243470f07bba5f3236d8a933c0824492d2b4
Belinda Liu <bliu@pivotal.io>
2020-05-27 18:01:48 +0000

    Add aliases for VAT GCP workstations
    Co-authored-by: Belinda Liu <bliu@pivotal.io>
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>

---
+++ custom-bash-it-plugins/vat-ws.bash
@@ -0,0 +1,3 @@
+alias vat-ws-1='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-1" --project "cf-cli"  --ssh-flag="-A"'
+alias vat-ws-2='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-2" --project "cf-cli"  --ssh-flag="-A"'
+alias vat-ws-3='gcloud beta compute ssh --zone "us-west1-b" "pivotal@vat-ws-3" --project "cf-cli"  --ssh-flag="-A"'
