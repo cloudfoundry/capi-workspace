b81aba645cb09f91ad70ad541dc3a8b97bd86063
Aakash Shah <ashah@pivotal.io>
2020-06-02 13:18:05 -0700

    Stop symlinking Brewfile into $HOME
    "brew bundle" accepts a "--file" param

--- brew-bundle.sh
+++ brew-bundle.sh
@@ -3,14 +3,9 @@
 set -e
 
 capi_brewfile="${PWD}/Brewfile"
-brewfile_link="${HOME}/.Brewfile"
-if [ ! -L "${brewfile_link}" ]; then
-  echo "Creating Brewfile symlink"
-  ln -s "${capi_brewfile}" "${brewfile_link}"
-fi
 
 echo "Installing from the Brewfile..."
 brew update || echo "brew update failed, but continuing"
 brew tap Homebrew/bundle
-brew bundle --global || true
+brew bundle --file "$capi_brewfile" || true
 brew cleanup
