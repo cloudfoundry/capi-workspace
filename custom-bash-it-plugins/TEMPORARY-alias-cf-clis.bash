3702a243186d5bcef57cc35bd15b1fcf81ebda0d
Connor Braa <cbraa@pivotal.io>
2020-06-15 14:13:06 -0700

    use a symlink for cf->cf7 so that baras can pick it up
    Authored-by: Connor Braa <cbraa@pivotal.io>

--- custom-bash-it-plugins/TEMPORARY-alias-cf-clis.bash
+++ custom-bash-it-plugins/TEMPORARY-alias-cf-clis.bash
@@ -1,3 +1,3 @@
 # TEMPORARY: until cf cli v7 is GA and updates their homebrew tap
-alias cf6='/usr/local/bin/cf'
-alias cf='/usr/local/bin/cf7'
+alias cf6='/usr/local/Cellar/cf-cli/6.51.0/bin/cf'
+ln -sf /usr/local/bin/cf7 /usr/local/bin/cf
