9529b241a1f0320b812a5ff31ecf5caf18326071
Matt Royal <mroyal@pivotal.io>
2020-06-19 10:58:09 -0700

    No more tiny dock
    Authored-by: Matt Royal <mroyal@pivotal.io>

--- setup/dock.sh
+++ setup/dock.sh
@@ -2,7 +2,5 @@
 
 set +e
 defaults delete com.apple.dock persistent-apps >/dev/null 2>&1 || true
-# Default height probably floor(160 / 2 - 11) = 69
-defaults write com.apple.dock tilesize -int 29
 killall Dock
 set -e
