4a26f71f6913eb98eeb69b6c3e1763604f1cd278
Aakash Shah <ashah@pivotal.io>
2020-06-19 17:49:29 -0700

    Rename scripts -> bin
    * This better reflects that this directory hold items to add to your PATH

--- lib/environment-variables.bash
+++ lib/environment-variables.bash
@@ -9,7 +9,7 @@ export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 
 # Add capi-workspace/scripts to PATH
-export PATH="$PATH:$HOME/workspace/capi-workspace/scripts"
+export PATH="$PATH:$HOME/workspace/capi-workspace/bin"
 
 # Add brew-installed python to the path.
 # Must go at the beginning so that it's prioritized over the system installation.
