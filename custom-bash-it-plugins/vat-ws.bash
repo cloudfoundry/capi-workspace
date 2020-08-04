b2f3859faa67ede8c403de777394db0cad9bd83d
Sarah Weinstein <sweinstein@pivotal.io>
2020-08-04 10:22:23 -0700

    Add `vat-ws-4` alias
    Co-authored-by: Sarah Weinstein <sweinstein@pivotal.io>
    Co-authored-by: Reid Mitchell <rmitchell@pivotal.io>

--- custom-bash-it-plugins/vat-ws.bash
+++ custom-bash-it-plugins/vat-ws.bash
@@ -1,3 +1,4 @@
 alias vat-ws-1='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-1" --project "cf-cli"  --ssh-flag="-A"'
 alias vat-ws-2='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-2" --project "cf-cli"  --ssh-flag="-A"'
 alias vat-ws-3='gcloud beta compute ssh --zone "us-west1-b" "pivotal@vat-ws-3" --project "cf-cli"  --ssh-flag="-A"'
+alias vat-ws-4='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-4" --project "cf-cli"  --ssh-flag="-A"'
