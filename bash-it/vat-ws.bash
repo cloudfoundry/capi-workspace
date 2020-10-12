9f31b1aa76c3a72134172487094022bf995f52f8
Seth Boyles <sboyles@pivotal.io>
2020-10-12 10:09:44 -0700

    ssh to capi-ws-1 with pivotal user so we can share tmux session
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Sarah Weinstein <sweinstein@pivotal.io>

--- bash-it/vat-ws.bash
+++ bash-it/vat-ws.bash
@@ -3,4 +3,4 @@ alias vat-ws-2='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-2
 alias vat-ws-3='gcloud beta compute ssh --zone "us-west1-b" "pivotal@vat-ws-3" --project "cf-cli"  --ssh-flag="-A"'
 alias vat-ws-4='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-4" --project "cf-cli"  --ssh-flag="-A"'
   
-alias capi-ws-1='gcloud beta compute ssh --zone "us-central1-a" "capi-ws-1" --project "cf-capi-arya"  --ssh-flag="-A"'
+alias capi-ws-1='gcloud beta compute ssh --zone "us-central1-a" "pivotal@capi-ws-1" --project "cf-capi-arya"  --ssh-flag="-A"'
