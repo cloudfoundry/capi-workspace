738fc3487846ebd46d3ce1ce673c40a3f9293548
Weyman Fung <weymanf@vmware.com>
2020-12-02 14:30:01 -0800

    add capi-ws-4 alias

--- bash-it/vat-ws.bash
+++ bash-it/vat-ws.bash
@@ -6,3 +6,4 @@ alias vat-ws-4='gcloud beta compute ssh --zone "us-central1-a" "pivotal@vat-ws-4
 alias capi-ws-1='gcloud beta compute ssh --zone "us-central1-a" "pivotal@capi-ws-1" --project "cf-capi-arya"  --ssh-flag="-A"'
 alias capi-ws-2='gcloud beta compute ssh --zone "us-central1-a" "pivotal@capi-ws-2" --project "cf-capi-arya"  --ssh-flag="-A"'
 alias capi-ws-3='gcloud beta compute ssh --zone "us-central1-a" "pivotal@capi-ws-3" --project "cf-capi-arya"  --ssh-flag="-A"'
+alias capi-ws-4='gcloud beta compute ssh --zone "us-central1-a" "pivotal@capi-ws-4" --project "cf-capi-arya"  --ssh-flag="-A"'
