d57753a447c7b319523e1e522a64d01bb91d19ec
Jenna Goldstrich <jgoldstrich@pivotal.io>
2021-11-02 18:36:54 +0000

    Remove brew handling of cli, add out/cf to path
    Co-authored-by: Jenna Goldstrich <jgoldstrich@pivotal.io>
    Co-authored-by: Michael Oleske <moleske@pivotal.io>

--- lib/environment-variables.bash
+++ lib/environment-variables.bash
@@ -11,6 +11,9 @@ export PATH=$PATH:$GOPATH/bin
 # Add capi-workspace/scripts to PATH
 export PATH="$PATH:$HOME/workspace/capi-workspace/bin"
 
+
+# Add CLI out directory to PATH
+export PATH="$PATH:$HOME/go/src/code.cloudfoundry.org/cli/out"
 # Add brew-installed python to the path.
 # Must go at the beginning so that it's prioritized over the system installation.
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
