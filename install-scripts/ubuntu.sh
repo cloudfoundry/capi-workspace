1a8ef36764e91f2389634489f96154760de33bdb
Seth Boyles <sboyles@pivotal.io>
2022-11-02 20:57:51 +0000

    fix yarn installation
    Co-authored-by: Seth Boyles <sboyles@pivotal.io>
    Co-authored-by: Michael Oleske <moleske@pivotal.io>

--- install-scripts/ubuntu.sh
+++ install-scripts/ubuntu.sh
@@ -20,8 +20,7 @@ sudo apt install -y \
   python-setuptools
 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
-  sudo apt-get install -y nodejs yarn
-
+  sudo apt-get install -y nodejs nginx && npm install yarn -g
 
 # fun dependencies
 sudo apt install -y \
