d98e734c6e60908ab3c05a221442a8300bfb4f5f
Seth Boyles <sboyles@vmware.com>
2023-02-13 22:52:38 +0000

    Change github clone to ssh from https
    Co-authored-by: Seth Boyles <sboyles@vmware.com>
    Co-authored-by: David Alvarado <alvaradoda@vmware.com>

--- set-up-new-machine.sh
+++ set-up-new-machine.sh
@@ -32,7 +32,7 @@ git clone https://github.com/cloudfoundry/capi-ci
 git clone https://github.com/cloudfoundry/cf-acceptance-tests
 git clone https://github.com/cloudfoundry/capi-bara-tests
 git clone https://github.com/cloudfoundry/sync-integration-tests
-git clone https://github.com/pivotal-cf/capi-env-pool
+git clone git@github.com:pivotal-cf/capi-env-pool.git
 
 # tmux setup with luan
 cd ~/workspace
