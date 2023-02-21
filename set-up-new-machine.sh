950fea154df3da2f8b2915a22c49f854df92c54d
David Alvarado <alvaradoda@vmware.com>
2023-02-21 23:15:45 +0000

    fix vim-nvim typo
    Co-authored-by: Michael Oleske <moleske@vmware.com>

--- set-up-new-machine.sh
+++ set-up-new-machine.sh
@@ -56,7 +56,7 @@ nvim -v
 
 #nvim plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir -p ~/.config/vim
+mkdir -p ~/.config/nvim
 ln -s ~/workspace/capi-workspace/init.vim ~/.config/nvim/init.vim
 nvim -es -i NONE -c "PlugInstall" -c "qa"
 
