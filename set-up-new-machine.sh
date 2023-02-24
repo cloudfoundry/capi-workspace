64f4b51953ec94e1bd27ac4edbe652986cd3ce49
Michael Oleske <moleske@vmware.com>
2023-02-24 00:53:18 +0000

    Take out bundle install cause the shell doesn't know ruby exists yet
    - probably should reload the shell

--- set-up-new-machine.sh
+++ set-up-new-machine.sh
@@ -187,11 +187,11 @@ alias gst="git status"
 EOF
 
 # prepare cloudcontroller_ng for running tests
-pushd ~/workspace/capi-release/src/cloud_controller_ng
-  bundle install
-  DB=mysql rake db:create
-  DB=postgres rake db:create
-popd
+#pushd ~/workspace/capi-release/src/cloud_controller_ng
+  #bundle install
+  #DB=mysql rake db:create
+  #DB=postgres rake db:create
+#popd
 
 # git alias some of the above scripts use and we like
 git config --global alias.ci commit
