056bc806056c1076506d0ddb2827b3348776f4b5
M. Oleske <moleske@pivotal.io>
2023-03-17 15:46:17 -0700

    Support fuzzy searching on 'up/down arrow' to previous commands

--- set-up-new-machine.sh
+++ set-up-new-machine.sh
@@ -56,6 +56,9 @@ sudo DEBIAN_FRONTEND=noninteractive apt install ./nvim-linux64.deb -y
 rm nvim-linux64.deb
 nvim -v
 
+# up/down arrow search history
+ln -s ~/workspace/capi-workspace/.inputrc ~/.inputrc
+
 # nvim plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim
