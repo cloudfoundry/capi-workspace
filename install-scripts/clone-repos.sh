9df4032b043ce556ba5773af9c84e132fb27a61a
David Alvarado <alvaradoda@vmware.com>
2023-09-27 21:23:00 +0000

    Update git clone script to reflect repos we care about
    Co-authored-by: Merric de Launey <mdelauney@vmware.com>

--- install-scripts/clone-repos.sh
+++ install-scripts/clone-repos.sh
@@ -38,22 +38,28 @@ pushd ~/workspace > /dev/null
 	clone git@github.com:cloudfoundry/capi-dockerfiles.git ~/workspace/capi-dockerfiles
 	clone git@github.com:cloudfoundry/capi-ci-private.git ~/workspace/capi-ci-private
 	clone git@github.com:cloudfoundry/cf-deployment.git ~/workspace/cf-deployment 
-	clone git@github.com:cloudfoundry/cf-for-k8s.git ~/workspace/cf-for-k8s 
 	clone git@github.com:cloudfoundry/cf-acceptance-tests.git ~/go/src/github.com/cloudfoundry/cf-acceptance-tests
 	clone git@github.com:cloudfoundry/sync-integration-tests.git ~/go/src/code.cloudfoundry.org/sync-integration-tests
 	clone git@github.com:cloudfoundry/capi-bara-tests.git ~/go/src/github.com/cloudfoundry/capi-bara-tests
+        clone git@github.com:pivotal-cf/lts-capi-release.git ~/workspace/lts-capi-release
+
 	clone git@github.com:cloudfoundry/cli.git ~/go/src/code.cloudfoundry.org/cli
 	
 	clone git@github.com:pivotal-cf/devex-performance-suite.git ~/workspace/devex-performance-suite
+
 	clone git@github.com:pivotal-cf/pcf-scheduler-release.git ~/workspace/pcf-scheduler-release
 	clone git@github.com:pivotal-cf/p-scheduler.git ~/workspace/p-scheduler
+	clone git@github.com:pivotal-cf/scheduler-ci.git ~/workspace/scheduler-ci.git
 
 	disable_cred_hook ~/workspace/capi-env-pool
 	disable_cred_hook ~/workspace/capi-ci-private
+
+        clone git@github.com:pivotal-cf/notifications-ui.git ~/go/src/github.com/pivotal-cf/notifications-ui
 
 	# clone golang repos and symlink them into the GOPATH
 	ln -sf	$HOME/go/src/github.com/cloudfoundry/cf-acceptance-tests ~/workspace/cf-acceptance-tests
 	ln -sfn $HOME/go/src/code.cloudfoundry.org/sync-integration-tests ~/workspace/sync-integration-tests
 	ln -sf	$HOME/go/src/github.com/cloudfoundry/capi-bara-tests ~/workspace/capi-bara-tests
+	ln -sf	$HOME/go/src/github.com/pivotal-cf/notifications-ui ~/workspace/notifications-ui
 	ln -sf	$HOME/go/src/code.cloudfoundry.org/cli/ ~/workspace/cli
 popd > /dev/null
