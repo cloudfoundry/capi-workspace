d93c3cb94cbbec5b0d14dc40d1d93943b69bfc7d
M. Oleske <moleske@users.noreply.github.com>
2023-11-17 15:43:10 -0800

    use non deprecated node 20 installation method

--- set-up-new-machine.sh
+++ set-up-new-machine.sh
@@ -13,9 +13,14 @@ function clone {
 	fi
 }
 
+# install node 20 from https://github.com/nodesource/distributions#installation-instructions
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 # dependencies to run tests
