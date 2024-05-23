41e3d270fc62c84100d4272a497091b020e14cdc
Greg Cobb <gcobb@vmware.com>
2024-05-23 14:21:48 -0700

    Tidy up target_bosh function
    - Mostly just adding some stdout lines to clarify what the command is
      doing

--- lib/target-bosh.bash
+++ lib/target-bosh.bash
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 target_bosh() {
-  # tput setaf 1 = red
-  # tput setaf 2 = green
-  # tput setaf 9 = reset color
-
   local env_pool="$HOME/workspace/capi-env-pool"
   local claimed_dir="$env_pool/bosh-lites/claimed"
 
+  echo "❯ Refreshing bosh lite pool state..."
   pushd ${claimed_dir} >/dev/null
-    git pull
+    git pull | sed 's/^/\t/'
   popd >/dev/null
 
   if [ -z "$1" ]; then
@@ -19,25 +16,30 @@ target_bosh() {
     env_file=${claimed_dir}/${1}
 
     if [ -f "$env_file" ]; then
+      echo "❯ Sourcing ${env_file} to set bosh environment variables..."
       source "$env_file"
-      env_ssh_key_path="${env_pool}/${1}/bosh.pem"
-      env_integration_config_path="${env_pool}/${1}/integration_config.json"
 
+      env_integration_config_path="${env_pool}/${1}/integration_config.json"
+      if [ ! -f "${env_integration_config_path}" ]; then
+        echo "❯ Writing a capi-specific integration_config.json..."
+        generate_integration_config > "${env_integration_config_path}"
+        echo -e "\tGenerated ${env_integration_config_path}. Enjoy!"
+      fi
+
+      env_ssh_key_path="${env_pool}/${1}/bosh.pem"
       if [ ! -f "${env_ssh_key_path}" ]; then
+        echo "❯ Writing a bosh gateway ssh key..."
         echo "${BOSH_GW_PRIVATE_KEY_CONTENTS}" > "${env_ssh_key_path}"
         chmod 0600 "${env_ssh_key_path}"
+        echo -e "\tGenerated ${env_ssh_key_path}. Enjoy!"
       fi
 
-      if [ ! -f "${env_integration_config_path}" ]; then
-        echo "writing ${1} a capi-specific integration_config.json..."
-        generate_integration_config > "${env_integration_config_path}"
-      fi
-
-      echo "$(tput setaf 2)Success!$(tput setaf 9)"
-
+      echo "❯ Setting BOSH_GW_PRIVATE_KEY, BOSH_ALL_PROXY, and CONFIG environment variables..."
       export BOSH_GW_PRIVATE_KEY="${env_ssh_key_path}"
       export BOSH_ALL_PROXY=ssh+socks5://${BOSH_GW_USER}@${BOSH_ENVIRONMENT}:22?private-key=${BOSH_GW_PRIVATE_KEY}
       export CONFIG="${env_integration_config_path}"
+
+      echo "$(tput setaf 2)Success!$(tput setaf 9)"
     else
       echo "$(tput setaf 1)Environment '${1}' does not exist. Valid environments are:$(tput setaf 9)"
       ls ${claimed_dir}
