440a0fc7b3ef31cf68c745cad9497fea5d41dc37
Greg Cobb <gcobb@vmware.com>
2024-06-05 15:49:57 -0700

    Additional minor tidying
    - Soften more directory structure opinions
    - Change some output formatting to be more consistent with other
      commands
    - Remove indenting output by piping to sed, since it doesn't work for
      non-stdout terminal output (e.g. bosh CLI output is often not
      pipe-able)

--- lib/target-bosh.bash
+++ lib/target-bosh.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 target_bosh() {
-  local env_pool="$HOME/workspace/capi-env-pool"
+  local env_pool="${CAPI_ENV_POOL_DIR:-$HOME/workspace/capi-env-pool}"
   local claimed_dir="$env_pool/bosh-lites/claimed"
 
-  echo "❯ Refreshing bosh lite pool state..."
+  printf "\nRefreshing bosh lite pool state...\n"
   pushd ${claimed_dir} >/dev/null
-    git pull | sed 's/^/\t/'
+    git pull
   popd >/dev/null
 
   if [ -z "$1" ]; then
@@ -16,25 +16,25 @@ target_bosh() {
     env_file=${claimed_dir}/${1}
 
     if [ -f "$env_file" ]; then
-      echo "❯ Sourcing ${env_file} to set bosh environment variables..."
+      echo "\nSourcing ${env_file} to set bosh environment variables...\n"
       source "$env_file"
 
       env_integration_config_path="${env_pool}/${1}/integration_config.json"
       if [ ! -f "${env_integration_config_path}" ]; then
-        echo "❯ Writing a capi-specific integration_config.json..."
+        echo "\nWriting a capi-specific integration_config.json...\n"
         generate_integration_config > "${env_integration_config_path}"
         echo -e "\tGenerated ${env_integration_config_path}. Enjoy!"
       fi
 
       env_ssh_key_path="${env_pool}/${1}/bosh.pem"
       if [ ! -f "${env_ssh_key_path}" ]; then
-        echo "❯ Writing a bosh gateway ssh key..."
+        printf "\nWriting a bosh gateway ssh key...\n"
         echo "${BOSH_GW_PRIVATE_KEY_CONTENTS}" > "${env_ssh_key_path}"
         chmod 0600 "${env_ssh_key_path}"
         echo -e "\tGenerated ${env_ssh_key_path}. Enjoy!"
       fi
 
-      echo "❯ Setting BOSH_GW_PRIVATE_KEY, BOSH_ALL_PROXY, and CONFIG environment variables..."
+      printf "\nSetting BOSH_GW_PRIVATE_KEY, BOSH_ALL_PROXY, and CONFIG environment variables...\n"
       export BOSH_GW_PRIVATE_KEY="${env_ssh_key_path}"
       export BOSH_ALL_PROXY=ssh+socks5://${BOSH_GW_USER}@${BOSH_ENVIRONMENT}:22?private-key=${BOSH_GW_PRIVATE_KEY}
       export CONFIG="${env_integration_config_path}"
